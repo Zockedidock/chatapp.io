f1fefab0e90ae2d464975e1ed6faaa3d589c5a8f
Joona Brückner <zockedidockhd@gmail.com>
2020-08-02 13:56:58 +0200

    Updates

---
+++ precache-manifest.6d54d00e19a63695abedda4ad9aff0ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6383e8a7704ea8c13498c706d22a1272",
+    "url": "/chatapp.io/index.html"
+  },
+  {
+    "revision": "90554eb8c49500ec48ee",
+    "url": "/chatapp.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1cd0ec0971afd379c5d9",
+    "url": "/chatapp.io/static/js/2.5d4c7800.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chatapp.io/static/js/2.5d4c7800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90554eb8c49500ec48ee",
+    "url": "/chatapp.io/static/js/main.3a6bf3a2.chunk.js"
+  },
+  {
+    "revision": "8d56129f5b13fe9ae3b7",
+    "url": "/chatapp.io/static/js/runtime-main.f4230312.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chatapp.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
